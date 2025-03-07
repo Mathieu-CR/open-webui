0fa041a81903613df4284f18b8ff5c0606facc7c
Mathieu9488 <mathieu.crespin@efrei.net>
2025-03-07 16:26:50 +0100

    flowise rebranding

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,8 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
-		APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
+		APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build'),
+		'import.meta.env.VITE_AWELS_HOST': JSON.stringify(process.env.VITE_AWELS_HOST || '192.168.1.10')
 	},
 	build: {
 		sourcemap: true
